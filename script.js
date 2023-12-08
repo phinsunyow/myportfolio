b365a9681a26616f766e273c66cf47bf158ac918
phinsunyow <153168778+phinsunyow@users.noreply.github.com>
2023-12-08 20:27:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Add any additional JavaScript functionality you need for your portfolio here
